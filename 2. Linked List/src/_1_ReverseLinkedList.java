02d54955d9614c9609cc0cf194b92214ec0b080d
Sujeet Kumar <100398418+Sujeet2898@users.noreply.github.com>
2022-09-15 13:08:58 +0530

    add

---
+++ 2. Linked List/src/_1_ReverseLinkedList.java
@@ -0,0 +1,37 @@
+/*
+Question: Reverse Linked List
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Input: head = [1,2]
+Output: [2,1]
+
+Input: head = []
+Output: []
+ */
+
+public class _1_ReverseLinkedList {
+
+    public class ListNode<T> {
+        T data;
+        ListNode<T> next;
+
+        ListNode(T data){
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, next = null;
+        while (head != null){
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
