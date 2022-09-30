0b5e8243dd699b39fc5801ddb2ab61dcd15895fd
Sujeet Kumar <100398418+Sujeet2898@users.noreply.github.com>
2022-09-30 08:47:00 +0530

    add

---
+++ 9. Stack and Queue/src/_3_ImplementStackUsingQueues.java
@@ -0,0 +1,75 @@
+/*
+Question: Implement Stack using Queues
+---------------------------------------
+Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions
+of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+void push(int x) : Pushes element x to the top of the stack.
+int pop() : Removes the element on the top of the stack and returns it.
+int top() : Returns the element on the top of the stack.
+boolean empty() : Returns true if the stack is empty, false otherwise.
+
+Notes:
+You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+
+Input
+["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 2, 2, false]
+
+Explanation
+MyStack myStack = new MyStack();
+myStack.push(1);
+myStack.push(2);
+myStack.top(); // return 2
+myStack.pop(); // return 2
+myStack.empty(); // return False
+
+Time Complexity: Only push is O(n), others are O(1).
+Space Complexity: O(2N) --> 2 queues are used
+
+ */
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _3_ImplementStackUsingQueues {
+
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public _3_ImplementStackUsingQueues() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        // Push x into q2
+        q2.add(x);
+
+        // Push all the elements of q1 to q2
+        while(!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+
+        //swap q1 and q2
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
+    public int pop() {
+        return q1.remove();
+    }
+
+    public int top() {
+        return q1.peek();
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
