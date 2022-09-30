501989251b4d135d2c284516c500711e6819fdfe
Sujeet Kumar <100398418+Sujeet2898@users.noreply.github.com>
2022-09-30 08:47:35 +0530

    add

---
+++ 9. Stack and Queue/src/_5_ImplementStackUsingOneQueue.java
@@ -0,0 +1,40 @@
+/*
+Question: Implement Stack using only one queue
+
+Time Complexity: Only push is O(n), others are O(1).
+Space Complexity: O(N) --> 1 queue is used
+
+ */
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _5_ImplementStackUsingOneQueue {
+
+    Queue<Integer> queue;
+
+    public _5_ImplementStackUsingOneQueue() {
+        queue = new LinkedList<>();
+
+    }
+
+    public void push(int x) {
+
+        queue.add(x);
+        for(int i = 0; i < queue.size() - 1; i++){
+            queue.add(queue.remove());
+        }
+    }
+
+    public int pop() {
+        return queue.remove();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
