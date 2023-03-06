b8e120f617a31d3e9613f997648f0c324a8831fa
Sujeet Kumar <100398418+Sujeet2898@users.noreply.github.com>
2023-03-06 08:18:48 +0530

    Update _2_PascalTriangle.java

--- 1. Arrays/src/_2_PascalTriangle.java
+++ 1. Arrays/src/_2_PascalTriangle.java
@@ -38,7 +38,7 @@ public class _2_PascalTriangle {
 /*
 Time Complexity: We are creating a 2D array of size (numRows * numCols) (where 1 <= numCols <= numRows),
 and we are traversing through each of the cells to update it with its correct value, so Time Complexity = O(numRows2).
-Space Complexity: Since we are creating a 2D array, space complexity = O(numRows2).
+Space Complexity: Since we are creating a 2D array, space complexity = O(numRows2)
  */
 
     public static ArrayList<ArrayList<Integer>> printPascal2(int n){
