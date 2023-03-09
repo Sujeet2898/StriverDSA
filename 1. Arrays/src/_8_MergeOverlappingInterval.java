065aac307b255fa563038743c3cb52ec3b1e8fbf
Sujeet Kumar <100398418+Sujeet2898@users.noreply.github.com>
2023-03-09 08:47:48 +0530

    Update _8_MergeOverlappingInterval.java

--- 1. Arrays/src/_8_MergeOverlappingInterval.java
+++ 1. Arrays/src/_8_MergeOverlappingInterval.java
@@ -5,7 +5,7 @@ of the non-overlapping intervals that cover all the intervals in the input.
 
 Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
 Output: [[1,6],[8,10],[15,18]]
-Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6]
 
 Input: intervals = [[1,4],[4,5]]
 Output: [[1,5]]
