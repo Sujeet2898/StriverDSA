5fe8641d69448cc47c93845c280c88297420488b
Sujeet Kumar <100398418+Sujeet2898@users.noreply.github.com>
2023-03-16 07:01:03 +0530

    Update _21_LongestConsecutiveSequence.java

--- 1. Arrays/src/_21_LongestConsecutiveSequence.java
+++ 1. Arrays/src/_21_LongestConsecutiveSequence.java
@@ -1,6 +1,6 @@
 /*
 Question: Longest Consecutive Sequence
-Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.   
 You must write an algorithm that runs in O(n) time.
 
 Input: nums = [100,4,200,1,3,2]
